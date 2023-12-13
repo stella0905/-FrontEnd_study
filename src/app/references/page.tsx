97332ce01fdf2691802cf33c95108af4a7ad8958
최유리 <choeyuli@gimgyeonglogs-MacBook-Pro.local>
2023-12-13 15:46:11 +0900

    기본 틀 작성

---
+++ src/app/references/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReferencesPage = () => {
+  return <div>레퍼런스 페이지</div>;
+};
+
+export default ReferencesPage;
---
+++ src/app/technology/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TechnologyPage = () => {
+  return (
+    <>
+      <h2> 목록 </h2>
+      <ul>
+        <li>javascript</li>
+      </ul>
+    </>
+  );
+};
+
+export default TechnologyPage;
